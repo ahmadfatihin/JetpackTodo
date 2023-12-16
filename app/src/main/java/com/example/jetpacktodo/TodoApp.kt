c7f7161073a8cd7ea055ab2ed416f3ecba392a0a
Fatih <ahmdfatihin17@gmail.com>
2023-12-16 11:52:20 +0700

    feat: implement and repo in todo app

--- app/src/main/java/com/example/jetpacktodo/TodoApp.kt
+++ app/src/main/java/com/example/jetpacktodo/TodoApp.kt
@@ -1,9 +1,23 @@
 package com.example.jetpacktodo
 
 import android.app.Application
+import androidx.room.Room
+import com.example.jetpacktodo.database.TodoDatabase
+import org.koin.core.context.startKoin
+import org.koin.dsl.bind
+import org.koin.dsl.module
 
 class TodoApp:Application() {
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            modules(module {
+                single {
+                    Room
+                        .databaseBuilder(this@TodoApp, TodoDatabase::class.java, "db")
+                        .build()
+                }
+            })
+        }
     }
 }
