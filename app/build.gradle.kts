4404985dd16b58b1ffd47c34071c63ba5b0ccc07
Fatih <ahmdfatihin17@gmail.com>
2023-12-23 06:11:43 +0700

    feat: add mock

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,14 +60,21 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    //Test
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
+    // MockK
+    testImplementation("io.mockk:mockk:1.12.0")
 
 
     val koinAndroidVersion = "3.4.0"
