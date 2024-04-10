082827cc73740367525800c2c1189ffde80069fd
Fatih <ahmdfatihin17@gmail.com>
2024-04-10 22:35:26 +0700

    Update Color.kt

--- app/src/main/java/com/example/jetpacktodo/ui/theme/Color.kt
+++ app/src/main/java/com/example/jetpacktodo/ui/theme/Color.kt
@@ -5,5 +5,5 @@ import androidx.compose.ui.graphics.Color
 val Dark200 = Color(0xff44475a)
 val Dark500 = Color(0xff2d2f3c)
 val Grey100 = Color(0xfffafafa)
-val Grey300 = Color(0xffe0e0e0)
-val Grey700 = Color(0xff616161)
+val Grey300 = Color(0xFF3C3B3B)
+val Grey700 = Color(0xFF2E2E2E)
